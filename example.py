39f3089f47aa2a312ea6de7d93ed251b1884bf20
priyang <bhattpriyang@gmail.com>
2024-11-14 10:57:40 +0530

    Initial Commit

---
+++ example.py
@@ -0,0 +1,14 @@
+import math  # unused import
+
+def greet(name):
+  print(f"Hello {name}")  # indentation
+
+
+
+
+def add(a,b):
+  return a+b     # need same between a+b
+
+
+greet("priyang")
+print(add(3,5))
